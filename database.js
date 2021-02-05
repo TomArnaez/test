07715557ec5e678f4c696840bf120caf6c0662c2
Christian Dobbie <chris@christiandobbie.com>
2021-02-05 10:42:14 +0000

    Create connection pool & attempt to run example SQL query and display result on index page (#8)

---
+++ database.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var config = require('./config/config.js');
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : config.mysql.host,
+    database        : config.mysql.name,
+    user            : config.mysql.user,
+    password        : config.mysql.pass
+});
+
+module.exports = pool;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  
+  getDatabaseVersion().then(function (result){
+    res.render('index', { title: 'Express' , databaseVersion: result });
+  });
+  
+
 });
 
 module.exports = router;
+
+
+function getDatabaseVersion() {
+  var db = require('../database.js');
+  return new Promise(function (resolve, reject){
+    db.query("SELECT VERSION() as VER", function (err, result) {
+      if (err) reject(err);
+      resolve(result[0].VER);
+    });
+  });
+}
