c5174eda0ce4ec19aa4b0e0e847faddc2970e918
TomArnaez <tomarnaez@gmail.com>
2021-02-07 01:34:35 +0000

    Add Bootstrap

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// bootstrap setup
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')))
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
