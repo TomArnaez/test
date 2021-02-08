cb3b1d369a5aec077e087f54bc1b9a71c06e93dc
Christian Dobbie <chris@christiandobbie.com>
2021-02-08 00:24:24 +0000

    Added ability to reset password if forgotten. (some duplicate code needs refactoring)

--- routes/admin_login.js
+++ routes/admin_login.js
@@ -86,6 +86,140 @@ router.post('/changepassword',(req,res,next)=>{
   }
 })
 
+router.get('/forgotpassword',(req,res)=>{
+  res.render('forgotpassword-request');
+})
+
+router.get('/forgotpassword/:token',(req,res)=>{
+  let token = req.params.token;
+  if(token != ""){
+    let jwt = require('jsonwebtoken');
+    let decodedUserID = "";
+    try {
+      decodedUserID = jwt.decode(token).id
+    }
+    catch(err) {
+      req.flash('error_msg','Invalid Token. Please try again.');
+      res.redirect('/admin/forgotpassword');
+      return;
+    }
+    db.query("SELECT id AS user_id, user_pass FROM users WHERE ? = id LIMIT 1;", [decodedUserID], function (err, result) {
+      if (err) {
+        req.flash('error_msg','No database connection.');
+        res.redirect('/admin/forgotpassword');
+      }
+      if(result.length > 0) {
+        jwt.verify(token, result[0].user_pass, function(err, decoded) {
+          if (err) {
+            req.flash('error_msg','Invalid Token. Please try again.');
+            res.redirect('/admin/forgotpassword');
+          } else {
+
+            //BEGIN GET UNIQUE CODE
+            res.render('forgotpassword-reset', { token: token });
+            //END GET UNIQUE CODE
+
+          }
+        });
+      } else {
+        req.flash('error_msg','Invalid Token. Please try again.');
+        res.redirect('/admin/forgotpassword');
+      }
+    });
+  } else {
+    res.redirect('/admin/forgotpassword');
+  }
+})
+
+router.post('/forgotpassword/:token',(req,res)=>{
+  let token = req.params.token;
+  if(token != ""){
+    let jwt = require('jsonwebtoken');
+    let decodedUserID = "";
+    try {
+      decodedUserID = jwt.decode(token).id
+    }
+    catch(err) {
+      req.flash('error_msg','Invalid Token. Please try again.');
+      res.redirect('/admin/forgotpassword');
+      return;
+    }
+    db.query("SELECT id AS user_id, user_pass FROM users WHERE ? = id LIMIT 1;", [decodedUserID], function (err, result) {
+      if (err) {
+        req.flash('error_msg','No database connection.');
+        res.redirect('/admin/forgotpassword');
+      }
+      if(result.length > 0) {
+        jwt.verify(token, result[0].user_pass, function(err, decoded) {
+          if (err) {
+            req.flash('error_msg','Invalid Token. Please try again.');
+            res.redirect('/admin/forgotpassword');
+          } else {
+
+            //BEGIN POST UNIQUE CODE
+            let newpass = req.body.newpass
+            let passmatch = req.body.newpass == req.body.newpass2
+            if(newpass == ""){
+              req.flash('error_msg','Missing field.');
+              res.redirect("/admin/forgotpassword/" + token);
+            } else if(!passmatch){
+              req.flash('error_msg','Passwords do not match.');
+              res.redirect("/admin/forgotpassword/" + token);
+            } else {
+              bcrypt.hash(newpass, 10, function(err, newhash) {
+                db.query("UPDATE users SET user_pass = ? WHERE id = ?;", [newhash, decodedUserID], function (err, result) {
+                  if (err) {
+                    req.flash('error_msg','No database connection.');
+                    res.redirect("/admin/forgotpassword/" + token);
+                  }
+                  req.flash('success_msg','Password updated! Please login:');
+                  res.redirect("/admin/login");
+                });
+              });
+            }
+            //END POST UNIQUE CODE
+
+          }
+        });
+      } else {
+        req.flash('error_msg','Invalid Token. Please try again.');
+        res.redirect('/admin/forgotpassword');
+      }
+    });
+  } else {
+    res.redirect('/admin/forgotpassword');
+  }
+})
+
+router.post('/forgotpassword',(req,res)=>{
+  if(req.body.username != ""){
+    db.query("SELECT id AS user_id, user_pass, user_email FROM users WHERE ? IN (user_login, user_email) LIMIT 1;", [req.body.username], function (err, result) {
+      if (err) {
+        req.flash('error_msg','No database connection.');
+        res.redirect('/admin/forgotpassword');
+      }
+      if(result.length > 0) {
+        let jwt = require('jsonwebtoken');
+        let token = jwt.sign({
+          id: result[0].user_id
+        }, result[0].user_pass, { expiresIn: '24h' });
+
+        console.log("Reset password for user '" + req.body.username + "' at [URL]/admin/forgotpassword/"+ token)
+
+        req.flash('success_msg','Password reset email has been sent.');
+        res.redirect('/admin/forgotpassword');
+
+      } else {
+        req.flash('error_msg','Invalid username or email.'); //do we really want to let a user know if a user exists/doesn't exist? Could send "If this user exists, a password reset has been sent" as same message as real user
+        res.redirect('/admin/forgotpassword');
+      }
+    });
+  } else {
+    req.flash('error_msg','Enter a username or email address.');
+    res.redirect('/admin/forgotpassword');
+  }
+})
+
 router.post('/register',(req,res)=>{
   req.flash('error_msg','You can not register.');
   res.redirect('/admin/login');
