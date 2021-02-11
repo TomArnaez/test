7ac5b13463f4cffce2e2c428d46acc17f2784e36
S Mohammad Reza <info@shahrestani.co.uk>
2021-02-11 22:12:44 +0000

    implement nodemailer

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 // const path = require('path');
+const bodyParser = require('body-parser');
+const expressHdl = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+
+const app = express();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,22 +15,65 @@ router.get('/', function(req, res, next) {
 });
 
 
-// app.use(express.urlencoded({
-//   extended: false
-// }));
-// app.use(express.json());
-
-//
-// app.post('/email', (req, res) => {
-//   // send the email
-//   res.json({message: 'WE GOT YOUR MESSAGE'})
-// });
-
-
-
-/* GET Messenger page. */
-router.get('/messenger', function(req, res) {
-  res.render('messenger');
+/* GET Contact page. */
+router.get('/contact', function(req, res) {
+  res.render('test.html');
 });
+
+
+
+// View engine setup
+app.engine('handlebars', expressHdl());
+app.set('view engine', 'handlebars');
+
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.post('/send', (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'informationhub.kings@gmail.com', //username
+      pass: 'cpointisthebest'  // password
+    },
+    tls:{
+      rejectUnauthorized:false  //bypass the security in localhost
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Information Hub Contact Centre" <informationhub.kings@gmail.com>', // sender address
+    to: 'test@test.com, informationhub.kings@gmail.com, info@shahrestani.co.uk', // list of receivers
+    subject: 'Contact Form', // Subject
+    text: 'Nothing for now!', // text body
+    html: output // html body
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) return console.log(error);
+
+    console.log('Message has been sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    res.render('contact', {msg:'Email has been sent'});
+  });
+});
+
 
 module.exports = router;
