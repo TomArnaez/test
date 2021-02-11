4b76d8bb876d21cd5b9cd9361b1e38a20c0247da
S Mohammad Reza <info@shahrestani.co.uk>
2021-02-11 21:03:36 +0000

    create messenger page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+// const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+// app.use(express.urlencoded({
+//   extended: false
+// }));
+// app.use(express.json());
+
+//
+// app.post('/email', (req, res) => {
+//   // send the email
+//   res.json({message: 'WE GOT YOUR MESSAGE'})
+// });
+
+
+
+/* GET Messenger page. */
+router.get('/messenger', function(req, res) {
+  res.render('messenger');
 });
 
 module.exports = router;
