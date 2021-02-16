2376b70b3eaa29f778ddc397242ffe9bc29727ab
Christian Dobbie <chris@christiandobbie.com>
2021-02-16 17:45:59 +0000

    Added comments & documentation.

--- passport.js
+++ passport.js
@@ -4,23 +4,24 @@ const db = require('./database.js');
 
 module.exports = function(passport) {
     passport.use(
-        new LocalStrategy((username, password, done)=> {
-            db.query("SELECT id AS user_id, user_pass FROM users WHERE ? IN (user_login, user_email) LIMIT 1;", [username], function (err, result) {
+        new LocalStrategy((username, password, done) => {
+            //Get user data from database
+            db.query("SELECT id AS user_id, user_pass FROM users WHERE ? IN (user_login, user_email) LIMIT 1;", [username], function(err, result) {
                 if (err) {
                     //throw(err);
-                    return done(null,false,{message : 'No database connection.'});
+                    return done(null, false, { message: 'No database connection.' });
                 }
-                if(result.length > 0) {
-                    bcrypt.compare(password, result[0].user_pass, function (err, hashMatched) {
-                        if(hashMatched){
+                if (result.length > 0) {
+                    //Check hash stored in database matches password provided.
+                    bcrypt.compare(password, result[0].user_pass, function(err, hashMatched) {
+                        if (hashMatched) { //Successful login.
                             return done(null, result[0].user_id);
                         } else {
-                            return done(null,false,{message : 'Invalid username/password'});
+                            return done(null, false, { message: 'Invalid username/password' });
                         }
                     })
-                }
-                else {
-                    return done(null,false,{message : 'Invalid username/password'});
+                } else {
+                    return done(null, false, { message: 'Invalid username/password' });
                 }
             });
         })
--- routes/admin_dashboard.js
+++ routes/admin_dashboard.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+// Example code to check user is authenticated. The logged in user id can be found in req.user. 
 router.get('/', function(req, res, next) {
-    if(req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         res.render('admin_dashboard', { user_id: req.user });
     } else {
-        req.flash('error_msg','You are not authenticated.');
+        req.flash('error_msg', 'You are not authenticated.');
         res.redirect("/admin/login");
     }
 });
