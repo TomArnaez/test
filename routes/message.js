7bf825f017d52a5c2be7205939b03f7c8f416c33
S Mohammad Reza <info@shahrestani.co.uk>
2021-02-18 23:22:33 +0000

    get unique id reference

--- routes/message.js
+++ routes/message.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const generateUniqueId = require('generate-unique-id');
 
 
-/* GET Contact page. */
+
+/* GET Message page. */
 router.get('/message', function(req, res) {
     res.render('message');
 });
 
+router.post('/message/send', function(req,res){
+    res.render('message', { message: 'Your Message Has Been Sent' , refNumber: getUniqueID() });
+});
+
+function getUniqueID(){
+    const reference = 'RE'
+    const id = generateUniqueId({
+        excludeSymbols: ['0'],
+        length: 7,
+        useLetters: false
+    }) ;
+    return reference.concat(id);
+}
+
+
+
 module.exports = router;
+
