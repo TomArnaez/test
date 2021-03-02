143ef6d06e5793046212bd7275eaa88eb62812c5
TomArnaez <tomarnaez@gmail.com>
2021-03-02 11:33:56 +0000

    Add timestamps to model

---
+++ models/term.model.js
@@ -0,0 +1,37 @@
+/**
+ * Terms are used for categorising posts. They belong to many posts and can
+ * have many labels. Need to create a post with tags? Then you might create a post
+ * with a 'tag' term:
+ *
+ * postTerms: [{
+ *    termType: tag,
+ *    termName: finance
+ *  },{
+ *    termType: tag,
+ *    termName: support group
+ *  }]
+ */
+
+module.exports = (sequelize, DataTypes) => {
+    const Term = sequelize.define('term', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        termType: {
+            type: DataTypes.STRING
+        },
+        termName: {
+            type: DataTypes.STRING
+        },
+        termSlug: {
+            type: DataTypes.STRING,
+            unique: true
+        }
+    }, {
+        timestamps: false
+    });
+    return Term;
+};
