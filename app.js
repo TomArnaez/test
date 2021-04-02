2b1fe598e5c0669f2bd394bb726012219a1af44b
Skye M <skyem@hotmail.co.uk>
2021-04-02 10:24:37 +0100

    Fix 404 error not being rendered properly

--- app.js
+++ app.js
@@ -24,10 +24,6 @@ const uploadRouter = media.uploadRoute
 
 const app = express();
 app.disable("x-powered-by");
-app.disable("x-powered-by");
-
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -36,6 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
 
@@ -81,7 +78,7 @@ app.use(function(req, res, next) {
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
